6070105c4b8c0bfe64bb2c902bee1d1cbf867f95
Denis Makaev <d.makaev@hightech.group>
2022-05-12 21:51:17 +0300

    Usage Example project

---
+++ ExampleProject/Ids.cs
@@ -0,0 +1,34 @@
+﻿using SimpleTypedValue;
+
+namespace ExampleProject;
+
+[TypedValue]
+public partial struct SomeId : ITypedValue<string>
+{
+    public static readonly string Default = string.Empty;
+    public string Value { get; }
+}
+
+public class BaseEntity<TID>
+    where TID : struct
+{
+    public TID Value { get; }
+}
+
+public partial class Organization : BaseEntity<Organization.ID>
+{
+    [TypedValue]
+    public partial struct ID : ITypedValue<long>
+    {
+        public long Value { get; }
+    }
+}
+
+public partial class User : BaseEntity<User.ID>
+{
+    [TypedValue]
+    public partial struct ID : ITypedValue<long>
+    {
+        public long Value { get; }
+    }
+}
